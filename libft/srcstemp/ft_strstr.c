74b4b2dfd5832940d4d8c4d6eefe1d2145a1b138
Tommy Cassu <90561323+TommyCassu@users.noreply.github.com>
2024-10-04 23:59:20 +0200

    strstr

---
+++ libft/srcstemp/ft_strstr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 23:58:50 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/04 23:58:50 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		j;
+	int		i;
+	char	*temp;
+
+	i = 0;
+	j = 0;
+	temp = str;
+	if (ft_strlen(str) < ft_strlen(to_find))
+		return (0);
+	else if (ft_strlen(to_find) == 0)
+		return (str);
+	while (temp[0] != '\0')
+	{
+		if (to_find[j] == 0)
+			return (temp - j);
+		else if (temp[0] == to_find[j])
+			j++;
+		else
+			j = 0;
+		temp++;
+	}
+	if (to_find[j] == 0)
+		return (temp - j);
+	return (0);
+}
