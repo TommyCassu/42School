d3d924707ad785344a5116b350bae5b4dde9c6a9
TommyVM <tommy.cassu@hotmail.com>
2024-10-05 12:41:25 +0200

    libft add more functions

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -4,12 +4,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* Basics functions */
 int ft_atoi(char *str);
-int ft_strcmp(char *s1, char *s2);
-int ft_strncmp(char *s1, char *s2, int n);
-int	ft_strlen(char *str);
 int tolower(int c);
 int toupper(int c);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+int ft_strcmp(char *s1, char *s2);
+int ft_strncmp(char *s1, char *s2, size_t n);
+int	ft_strlen(char *str);
+char	*ft_strstr(char *str, char *to_find);
+char	*ft_strchr(const char *s, int c);
+char    *ft_strcpy(char *dest, char *src);
+char    *ft_strncpy(char *dest, char *src, size_t n);
 char    *ft_strcat(char *dest, char *src);
+char    *ft_strncat(char *dest, char *src, size_t n);
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+/* Memory functions */
+void    *ft_memchr(const void *s, int c, size_t n);
+void    *ft_memset(void *s, int c, size_t n);
+char    *ft_strdup(const char *str);
 
 #endif
