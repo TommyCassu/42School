098b338b646f1d66f4b3bafcf141e879cfca3ede
TommyVM <tommy.cassu@hotmail.com>
2024-11-06 23:21:51 +0100

    fix strncmp

--- libft/srcs/ft_strncmp.c
+++ libft/srcs/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcassu <tcassu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: toto <toto@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:51:09 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/04 20:35:02 by tcassu           ###   ########.fr       */
+/*   Updated: 2024/11/06 23:21:21 by toto             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 {
 	size_t	i;
 
+	
 	i = 0;
-	while (s1[i] != '\0' && s1[i] == s2[i] && i < (n - 1))
+	if (n == 0)
+        return 0;
+	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	return (s1 - s2);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
